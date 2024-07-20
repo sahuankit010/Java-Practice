685a022a03f5f7eab8d9ff0534b0ff24bd79b376
sahuankit010 <sahu.ankit010@gmail.com>
2024-07-20 18:34:05 -0500

    made changes to follow standard java conventions

--- src/graphs/Graph.java
+++ src/graphs/Graph.java
@@ -5,33 +5,58 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This class represents a graph using an adjacency list.
+ */
 public class Graph {
 
-    static Map<Integer, List<Integer>> graph;
+    private final Map<Integer, List<Integer>> graph;
+
+    /**
+     * Constructor to initialize the graph.
+     */
     public Graph() {
-        getGraph();
+        this.graph = initializeGraph();
     }
 
-    static Map<Integer, List<Integer>> getGraph(){
-
-        graph = new HashMap<>();
+    /**
+     * Initializes the graph with predefined edges.
+     *
+     * @return the graph represented as a map
+     */
+    private Map<Integer, List<Integer>> initializeGraph() {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
         graph.put(0, Arrays.asList(1, 2));
         graph.put(2, Arrays.asList(3));
         graph.put(1, Arrays.asList(5, 2));
         graph.put(6, Arrays.asList(1, 5));
         graph.put(5, Arrays.asList(3, 4));
-        printGraph();
+        printGraph(graph);
         return graph;
     }
 
-    static void printGraph(){
+    /**
+     * Prints the adjacency list of the graph.
+     *
+     * @param graph the graph to be printed
+     */
+    private void printGraph(Map<Integer, List<Integer>> graph) {
         System.out.println("Adjacency List is:");
-        for(int k: graph.keySet()){
+        for (int k : graph.keySet()) {
             System.out.print(k + ": ");
-            for(int v: graph.get(k)){
+            for (int v : graph.get(k)) {
                 System.out.print(v + ", ");
             }
             System.out.println();
         }
+    }
+
+    /**
+     * Returns the graph.
+     *
+     * @return the graph as a map
+     */
+    public Map<Integer, List<Integer>> getGraph() {
+        return this.graph;
     }
 }
--- src/graphs/TopologicalSort.java
+++ src/graphs/TopologicalSort.java
@@ -2,22 +2,38 @@ package graphs;
 
 import java.util.*;
 
+/**
+ * This class provides functionality to perform topological sorting on a graph.
+ */
 public class TopologicalSort {
+
+    /**
+     * Main method to execute the topological sort.
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
-        Map<Integer, List<Integer>> graph = Graph.getGraph();
+        Graph graphInstance = new Graph();
+        Map<Integer, List<Integer>> graph = graphInstance.getGraph();
         List<Integer> result = topSort(graph);
         System.out.println("Topological Sort is: ");
-        for(int node: result){
+        for (int node : result) {
             System.out.print(node + " ");
         }
     }
 
-    private static List<Integer> topSort(Map<Integer, List<Integer>> graph){
+    /**
+     * Performs topological sort on the given graph.
+     *
+     * @param graph the graph to be sorted
+     * @return a list representing the topologically sorted order of nodes
+     */
+    private static List<Integer> topSort(Map<Integer, List<Integer>> graph) {
         Deque<Integer> sortedNodes = new ArrayDeque<>();
         Set<Integer> visited = new HashSet<>();
 
-        for(int node: graph.keySet()){
-            if(!visited.contains(node)){
+        for (int node : graph.keySet()) {
+            if (!visited.contains(node)) {
                 dfs(graph, node, visited, sortedNodes);
             }
         }
@@ -25,12 +41,20 @@ public class TopologicalSort {
         return new ArrayList<>(sortedNodes);
     }
 
+    /**
+     * Depth-first search to visit nodes in topological order.
+     *
+     * @param graph       the graph
+     * @param startNode   the starting node
+     * @param visited     the set of visited nodes
+     * @param sortedNodes the stack to hold the sorted nodes
+     */
     private static void dfs(Map<Integer, List<Integer>> graph, int startNode, Set<Integer> visited, Deque<Integer> sortedNodes) {
         visited.add(startNode);
 
-        if(graph.containsKey(startNode)){
-            for(int neighbor: graph.get(startNode)){
-                if(!visited.contains(neighbor)){
+        if (graph.containsKey(startNode)) {
+            for (int neighbor : graph.get(startNode)) {
+                if (!visited.contains(neighbor)) {
                     dfs(graph, neighbor, visited, sortedNodes);
                 }
             }
